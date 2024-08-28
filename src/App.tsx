d9219837a6bd870b13bd1f445bfc712121a599b7
dibya.borah <dibya.borah@taazaa.com>
2024-08-29 02:07:30 +0530

    done counter app using zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { create } from "zustand";
+import { Button } from "./components/ui/button"
+
+const useStore = create<{
+  count: number,
+  decQty: () => void,
+  incQty: () => void,
+}>((set) => ({
+  count: 0,
+  decQty: () => set((state) => ({ count: state.count - 1 })),
+  incQty: () => set((state) => ({ count: state.count + 1 })),
+}))
 
 const App = () => {
+  const { count, incQty, decQty } = useStore((store) => store);
   return (
-    <div>App</div>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Button onClick={decQty}> - </Button>
+      {count}
+      <Button onClick={incQty}> + </Button>
+    </div>
   )
 }
 
