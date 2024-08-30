6412ea74f1c595faf9459a57f45cf59417a76395
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-30 22:57:58 +0530

    Total amount is reflected and changed very smoothly

--- src/components/ChangeQtyButtons.tsx
+++ src/components/ChangeQtyButtons.tsx
@@ -2,16 +2,32 @@ import { useStore } from "@/store/store";
 import { useShallow } from "zustand/react/shallow";
 import { Button } from "./ui/button";
 import { Minus, Plus } from "lucide-react";
+import { useEffect } from "react";
 
 type Props = { productId: string };
 
 export function ChangeQtyButtons({ productId }: Props) {
-    const { decQty, incQty, getProductById } = useStore(useShallow((state) => ({
+    const { decQty, incQty, getProductById, setTotal } = useStore(useShallow((state) => ({
         getProductById: state.getProductById,
         decQty: state.decQty,
         incQty: state.incQty,
-    })))
+        setTotal: state.setTotal,
+    })));
+
     const product = getProductById(productId);
+
+    useEffect(() => {
+        const unSubscribe = useStore.subscribe((state) => state.products,
+         (products) => {
+            setTotal(products.reduce((accumulator, currentItem) => {
+                return accumulator + currentItem.price * currentItem.qty;
+            }, 0));
+        },
+        { fireImmediately : true },// This line is just to make things work more smoothly // That's only for now
+    ); 
+    return unSubscribe;
+    }, [setTotal]);
+
     return <>
         { 
             product && (
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,11 +3,13 @@ import { createUserSlice } from "./user-slice";
 import { Store } from "@/types/store";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cart-slice";
-import { devtools } from "zustand/middleware";
+import { devtools, subscribeWithSelector } from "zustand/middleware";
 
 export const useStore = create<Store>()(
-    devtools(immer((...a) => ({ // a is the setter and getter utities that the store provides us
-    ...createUserSlice(...a),
-    ...createCartSlice(...a),
-}))));
+    devtools(
+      subscribeWithSelector(
+        immer((...a) => ({ // a is the setter and getter utities that the store and create method provides us
+          ...createUserSlice(...a),
+          ...createCartSlice(...a),
+})))));
  
