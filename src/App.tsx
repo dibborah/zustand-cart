56afc9461cec91ea611745d093349dbd84f3c1b2
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-30 21:39:06 +0530

    Cards content are rendered with buttons which are dynamically changing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { Card, CardHeader } from "./components/ui/card";
+import { Button } from "./components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "./components/ui/card";
 import { PRODUCTS_DATA } from "./lib/mockData";
+import { useStore } from "./store/store";
 
 export default function App() {
+  const addProduct = useStore((state) => state.addProduct);
+  const cartProducts = useStore((state) => state.products);
   return (
     <main className="space-y-2 dark h-screen bg-background max-w-sm mx-auto mt-2">
       <h1 className="text-2xl">Products:</h1>
       <div className="space-y-2">
         {PRODUCTS_DATA.map((product) => <Card key={product.id}>
           <CardHeader>{product.title}</CardHeader>
+          <CardContent>{product.price}$</CardContent>
+          <CardFooter>
+            {cartProducts.find((item) => item.id === product.id) ? (
+              <>change qty buttons</>
+            ) : (
+              <Button onClick={() => addProduct(product)} variant="default">Add to Cart</Button>
+            )}
+          </CardFooter>
         </Card>)}
       </div>
     </main>
