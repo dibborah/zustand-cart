82ab37773a57713eb06520f267dd9a36f9ad92a9
dibya.borah <dibya.borah@taazaa.com>
2024-08-30 20:36:25 +0530

    Whole store creation fully finished

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,9 @@ import { create } from "zustand";
 import { createUserSlice } from "./user-slice";
 import { Store } from "@/types/store";
 import { immer } from "zustand/middleware/immer";
+import { createCartSlice } from "./cart-slice";
 
 export const useStore = create<Store>()(immer((...a) => ({ // a is the setter and getter utities that the store provides us
-    ...createUserSlice(...a)
+    ...createUserSlice(...a),
+    ...createCartSlice(...a),
 })));
