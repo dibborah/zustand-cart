f7ef558f700f5a4792750132d639aefdbbef4dc6
dibya.borah <dibya.borah@taazaa.com>
2024-08-30 21:03:49 +0530

    Learned different ways to access the store data and create a mock data file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { create } from "zustand";
-import { Button } from "./components/ui/button"
+export default function App() {
 
-const useStore = create<{
-  count: number,
-  decQty: () => void,
-  incQty: () => void,
-}>((set) => ({
-  count: 0,
-  decQty: () => set((state) => ({ count: state.count - 1 })),
-  incQty: () => set((state) => ({ count: state.count + 1 })),
-}))
-
-const App = () => {
-  const { count, incQty, decQty } = useStore((store) => store);
-  return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Button onClick={decQty}> - </Button>
-      {count}
-      <Button onClick={incQty}> + </Button>
+  return <>
+    <div>
     </div>
-  )
-}
-
-export default App
+  </>
+};
