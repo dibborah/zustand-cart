053001a517fe81637a5d05c24848948c1b153dd0
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-30 23:36:17 +0530

    persist localstorage is added in our zustand store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,21 @@ import { createUserSlice } from "./user-slice";
 import { Store } from "@/types/store";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cart-slice";
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 
 export const useStore = create<Store>()(
-    devtools(
+  devtools(
+    persist(
       subscribeWithSelector(
-        immer((...a) => ({ // a is the setter and getter utities that the store and create method provides us
+        immer((...a) => ({
+          // a is the setter and getter utities that the store and create method provides us
           ...createUserSlice(...a),
           ...createCartSlice(...a),
-})))));
- 
+        }))
+      ),
+      {
+        name: "zustand-store",
+      }
+    )
+  )
+);
