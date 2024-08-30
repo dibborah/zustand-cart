cd02ce1d4419a5d1fad09f4e0f6bdfb26938fff3
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-30 22:11:04 +0530

    intergrated devtools middleware in our store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,8 +3,11 @@ import { createUserSlice } from "./user-slice";
 import { Store } from "@/types/store";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cart-slice";
+import { devtools } from "zustand/middleware";
 
-export const useStore = create<Store>()(immer((...a) => ({ // a is the setter and getter utities that the store provides us
+export const useStore = create<Store>()(
+    devtools(immer((...a) => ({ // a is the setter and getter utities that the store provides us
     ...createUserSlice(...a),
     ...createCartSlice(...a),
-})));
+}))));
+ 
