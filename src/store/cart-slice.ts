5b174b36b0624509facdb0c75227132d3f558bb3
dibya.borah <dibya.borah@taazaa.com>
2024-08-30 20:32:48 +0530

    All method are defined in store

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -29,13 +29,43 @@ export const createCartSlice: StateCreator<
   [['zustand/immer', never]],
   [],
   CartSlice
-> = ( set ) => ({
+> = ( set, get ) => ({
     ...inititalState,
-    incQty: (productId) => set((state) => {
-        const foundProduct = state.products.find((product) => product.id === productId);
+    incQty: (productId) => 
+        set((state) => {
+        const foundProduct = state.products.find(
+            (product) => product.id === productId
+        );
         if(foundProduct) {
             foundProduct.qty += 1;
         }
-    })
+    }),
+    decQty: (productId) => set((state) => {
+        const foundIndex = state.products.findIndex(
+            (product) => product.id === productId
+        );
+        if(foundIndex !== -1) {
+            if(state.products[foundIndex].qty === 1){
+                state.products.splice(foundIndex, 1);
+            } else {
+                state.products[foundIndex].qty -= 1;
+            }
+        }
+    }),
+    addProduct: (product) => set((state) => {
+        state.products.push({...product, qty: 1});
+    }),
+    removeProduct: (productId) => set((state) => {
+        state.products = state.products.filter((product) => {
+            return product.id !== productId
+        });
+    }),
+    getProductById: (productId) => 
+      get().products.find((product) => product.id === productId),
+    setTotal: (total) =>
+        set((state) => {
+            state.total = total;
+        }),
+    reset: () => set(() => inititalState),
 });
 
